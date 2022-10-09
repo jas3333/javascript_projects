42cc722d3e80cf45c9898b1bc5e047dc03b7eef0
jas3333 <accounts1@protonmail.com>
2022-10-09 14:07:10 -0700

    Fixed a bug where you couldnt resume timer from a stop

--- timer/scripts/script.js
+++ timer/scripts/script.js
@@ -2,42 +2,42 @@
 
 function startTimer() {
 
-	if (timer.seconds !== -1) {
-		timer.setTimer();
-	}
-	else {
-		return;
-	}
+    if (timer.seconds !== -1) {
+        timer.setTimer();
+    }
+    else {
+        return;
+    }
 
-	timer.seconds -= 1;
+    timer.seconds -= 1;
 
 }
 
 function setTimer(timer) {
-	return {
-		setTimer: () => {
-			let minutes = Math.floor(timer.seconds / 60);
-			let seconds = Math.floor(timer.seconds % 60);
+    return {
+        setTimer: () => {
+            let minutes = Math.floor(timer.seconds / 60);
+            let seconds = Math.floor(timer.seconds % 60);
 
-			seconds < 10 ? seconds = "0" + seconds : seconds;
-			timer.domProperties.domTimer.textContent = `${minutes}:${seconds}`;
-		}
-	};
+            seconds < 10 ? seconds = "0" + seconds : seconds;
+            timer.domProperties.domTimer.textContent = `${minutes}:${seconds}`;
+        }
+    };
 }
 
 
 function createTimer() {
-	let getSeconds = document.getElementById("enter-seconds").value;
+    let getSeconds = document.getElementById("enter-seconds").value;
 
-	let timer = {
-		seconds: getSeconds,
-		minutes: 0,
-		domProperties: {
-			domTimer: document.getElementById("timer-text"),
-		}
-	};
+    let timer = {
+        seconds: getSeconds,
+        minutes: 0,
+        domProperties: {
+            domTimer: document.getElementById("timer-text"),
+        }
+    };
 
-	return Object.assign(timer, setTimer(timer));
+    return Object.assign(timer, setTimer(timer));
 }
 
 
@@ -47,22 +47,25 @@ const resetButton = document.getElementById("reset-timer");
 
 let timer = createTimer();
 let interval;
+
 startButton.onclick = function () {
 
-	timer.seconds = document.getElementById("enter-seconds").value;
+    if (timer.seconds < 1)
+        timer.seconds = document.getElementById("enter-seconds").value;
 
-	clearInterval(interval);
-	interval = setInterval(startTimer, 1000);
 
-	stopButton.onclick = function () {
-		clearInterval(interval);
-	};
+    clearInterval(interval);
+    interval = setInterval(startTimer, 1000);
 
-	resetButton.onclick = function () {
-		clearInterval(interval);
-		timer.seconds = 0;
-		timer.setTimer();
-	};
+    stopButton.onclick = function () {
+        clearInterval(interval);
+    };
+
+    resetButton.onclick = function () {
+        clearInterval(interval);
+        timer.seconds = 0;
+        timer.setTimer();
+    };
 };
 
 
